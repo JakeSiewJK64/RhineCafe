b0593f2e95bbba8733989f8f5dcbd44fa31b43ce
Siew Joe Kane <joekanesiew@outlook.com>
2021-08-13 21:18:09 +0800

    updated routing span

--- src/app/github-tutorial/github-tutorial.component.ts
+++ src/app/github-tutorial/github-tutorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-tutorial',
@@ -7,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubTutorialComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
+  }
+
+  navigateToGithubPR() {
+    this.router.navigate(['github-pr']);
   }
 
 }
