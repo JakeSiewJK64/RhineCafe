f5250210f93fa1b2e0a01b112c4b6b72e781e68c
Siew Joe Kane <joekanesiew@outlook.com>
2021-08-15 20:42:33 +0800

    updated about page

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { BioDescriptionEN } from '../shared/english.constants';
+import { Router } from '@angular/router';
+import { TdDialogService } from '@covalent/core/dialogs';
+import { BioDescriptionEN, DisplayLanguage, UsernameEN } from '../shared/english.constants';
+import { UsernameJP, BioDescriptionJP } from '../shared/japanese.constants';
+import { EventEmitterService } from '../shared/services/event-emitter.service';
 
 @Component({
   selector: 'app-about-page',
@@ -7,12 +11,29 @@ import { BioDescriptionEN } from '../shared/english.constants';
   styleUrls: ['./about-page.component.scss']
 })
 export class AboutPageComponent implements OnInit {
+  isEN: boolean = false;
 
-  constructor() { }
+  constructor(private dialogService: TdDialogService,
+    private router: Router,
+    private eventEmitterService: EventEmitterService) {
+  }
 
-  bio: string = BioDescriptionEN;
+  username: string = UsernameEN;
+  description: string = BioDescriptionEN;
 
   ngOnInit(): void {
+    this.eventEmitterService.eventEmitter.subscribe(x => this.load());
+    this.load();
+  }
+
+  checkLanguage() {
+    this.isEN = !(localStorage.getItem(DisplayLanguage) === 'en');
+  }
+
+  load() {
+    this.checkLanguage();
+    this.username = !this.isEN ? UsernameJP : UsernameEN;
+    this.description = !this.isEN ? BioDescriptionJP : BioDescriptionEN;
   }
 
 }
